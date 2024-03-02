9eea2bf047cea4a58c9f74de7060139ae1e0ba48
Isaac Diamond <isaacd9@gmail.com>
2024-03-02 13:37:41 -0800

    wip joins

---
+++ join.go
@@ -0,0 +1,42 @@
+package streams
+
+import "context"
+
+type TableReader[K comparable, V any] interface {
+	Reader[K, V]
+	get(key K) (V, error)
+}
+
+type JoinReader[K comparable, V, VJoin, VOut any] struct {
+	r      Reader[K, V]
+	t      TableReader[K, VJoin]
+	joiner func(Record[K, V], VJoin) VOut
+}
+
+func (j *JoinReader[K, V, VJoin, VOut]) Read(ctx context.Context) (Record[K, VOut], error) {
+	msg, err := j.r.Read(ctx)
+	if err != nil {
+		return Record[K, VOut]{}, nil
+	}
+
+	join, err := j.t.get(msg.Key)
+	if err != nil {
+		return Record[K, VOut]{}, nil
+	}
+
+	out := j.joiner(msg, join)
+
+	return Record[K, VOut]{
+		Key:   msg.Key,
+		Value: out,
+		Time:  msg.Time,
+	}, nil
+}
+
+func Join[K comparable, V, VJoin, VOut any](r Reader[K, V], t TableReader[K, VJoin], joiner func(Record[K, V], VJoin) VOut) Reader[K, VOut] {
+	return &JoinReader[K, V, VJoin, VOut]{
+		r:      r,
+		t:      t,
+		joiner: joiner,
+	}
+}
