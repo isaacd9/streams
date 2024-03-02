78d3e387f1a66d2beedc8d7b5f17b55cb29c7fbe
Isaac Diamond <isaacd9@gmail.com>
2024-03-02 12:27:39 -0800

    read entire state

--- state.go
+++ state.go
@@ -4,6 +4,7 @@ package streams
 type State[K comparable, V any] interface {
 	Get(k K) (V, error)
 	Put(k K, v V) error
+	Each(func(K, V) error) error
 }
 
 type MapState[K comparable, V any] struct {
@@ -17,6 +18,15 @@ func (m *MapState[K, V]) Get(k K) (V, error) {
 
 func (m *MapState[K, V]) Put(k K, v V) error {
 	m.m[k] = v
+	return nil
+}
+
+func (m *MapState[K, V]) Each(f func(K, V) error) error {
+	for k, v := range m.m {
+		if err := f(k, v); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
